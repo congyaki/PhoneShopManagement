7100ab40d6feb165959ab718af24d082fd21b4c8
congyaki <duccong29092003@gmail.com>
2023-05-01 23:16:03 +0700

    Add project files.

---
+++ PhoneShopManagement/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhoneShopManagement.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
