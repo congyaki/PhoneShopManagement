07619c144ef1d8d67fbb83a093bf9d9184a66f71
congyaki <duccong29092003@gmail.com>
2023-05-01 23:26:58 +0700

    Bi loi sau khi khoi tao Area

--- PhoneShopManagement/Program.cs
+++ PhoneShopManagement/Program.cs
@@ -20,13 +20,14 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+//Loi tai day ----------------
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(
         name: "areas",
         pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
     );
-
+    
     endpoints.MapControllerRoute(
         name: "default",
         pattern: "{controller=Home}/{action=Index}/{id?}"
